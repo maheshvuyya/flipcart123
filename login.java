128a08682c3e3db53eac6e711299e38d3f4df0ca
mahesh <mahesh@Ubuntu.myguest.virtualbox.org>
2023-03-16 11:39:38 +0530

    we write sum opreation

--- login.java
+++ login.java
@@ -1,13 +1,15 @@
 
 //sample program
 
-public class Demo
+public class login
 {
-public static void main(String[] args
-
-
-
-)
-{
+	public static void main(String[] args)
+	{
+		System.out.println("WELLCOME TO DEVOPS");
+              
+	int a=10,b=20;
+       int c;
+          c=a+b;
+   System.out.println(c);
 }
 } 
