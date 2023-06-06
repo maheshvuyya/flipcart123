6c73eb5163e8266424a82011e838048efb5d19fc
mahesh <mahisaiv1151@gmail.com:qgit config --global user.email mahisaiv1151@gmail.com>
2023-06-06 14:03:35 +0530

    wellcome

--- login.java
+++ login.java
@@ -6,7 +6,8 @@ public class login
 	public static void main(String[] args)
 	{
 		System.out.println("WELLCOME TO DEVOPS");
-              
+		System.out.println("i am in nellore");
+
 	int a=10,b=20,d=100123;
 
        int c;
