a809dee539f7f1dd3246e63e0f95e177a92cd362
mahesh <mahisaiv1151@gmail.com:qgit config --global user.email mahisaiv1151@gmail.com>
2023-06-06 14:06:36 +0530

    file name chnaged

---
+++ login1.java
@@ -0,0 +1,18 @@
+
+//sample program
+
+public class login
+{
+	public static void main(String[] args)
+	{
+		System.out.println("WELLCOME TO DEVOPS");
+		System.out.println("i am in nellore");
+
+	int a=10,b=20,d=100123;
+
+       int c;
+          c=a+b;
+   System.out.println("Addition for a and b:"+c);
+}
+}
+
